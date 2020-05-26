f1c72c4e1c61245d394ec61e58a13f08d628a1ed
Eric Daniel <ericdanielfb@gmail.com>
2020-05-26 12:02:49 -0300

    feature: search in podcasts

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 let Parser = require('rss-parser');
-// let convert = require('xml-js');
 
 let parser = new Parser();
-let url = 'https://jovemnerd.com.br/feed-nerdcast/';
+// let url = 'https://jovemnerd.com.br/feed-nerdcast/';
 // let url = 'https://www.rpgnext.com.br/category/tarrasque-na-bota/feed/';
 // let url = 'https://audio.globoradio.globo.com/podcast/feed/702/o-assunto';
-// let url = 'https://hipsters.tech/feed/podcast/';
+let url = 'https://hipsters.tech/feed/podcast/';
 
 
 async function init() {
     let feed = await (await parser.parseURL(url)).items;
     // console.log(feed);
     // console.log(feed[0].enclosure.url);
-    console.log(feed[0].itunes.image);
+    // console.log(feed[0].itunes.image);
+    search('java', feed);
 }
 
 
-function searchTitle(title, feed){
-
+function search(term, feed) {
+    let search = term.toUpperCase();
+    let results = feed.filter(res => {
+        return res.title && res.content &&
+            (res.title.toUpperCase().includes(search) ||
+                res.content.toUpperCase().includes(search));
+    });
+    return results;
 }
 
 
